b12ed64fadfae793f782b834ca0560eb9eea1156
yohan <noyohanx@naver.com>
2022-10-10 23:57:40 +0900

    delete serializers comment

--- config/serializers.py
+++ config/serializers.py
@@ -3,5 +3,5 @@ from .models import User # 선언한 모델
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User # 선언한 모델을 model 값에 넣어주는 작업
-        fields = ('id','username','age','city') # 필드 설정
+        model = User
+        fields = ('id','username','age','city')
