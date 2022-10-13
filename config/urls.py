1c23803d25f2dcd5f9a705498a81fed553125fa6
yohan <noyohanx@naver.com>
2022-10-13 17:41:29 +0900

    add addresses/int:pk

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,6 @@ from tutorial_1 import views
 
 urlpatterns = [
     path('addresses/', views.address_list),
+    path('addresses/<int:pk>/', views.address),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
