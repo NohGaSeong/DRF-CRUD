f1b318c1b32c502730063c87fd20476079a0a2a0
yohan <noyohanx@naver.com>
2022-10-13 13:57:02 +0900

    create Address Model

--- tutorial_1/models.py
+++ tutorial_1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    name = models.CharField(max_length=10)
+    phoneNumber = models.CharField(max_length=15)
+    address = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
+
