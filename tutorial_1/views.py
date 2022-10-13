8bdad864de73d99e4ac5ed6fa347ce8e84505ab5
yohan <noyohanx@naver.com>
2022-10-13 14:48:06 +0900

    create address_list view

--- tutorial_1/views.py
+++ tutorial_1/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .models import Address
+from .serializers import AddressSerializers
 
-# Create your views here.
+def address_list(request):
+    if request.method == 'GET':
+        query_set = Address.objects.all()
+        serializer = AddressSerializers(query_set, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = AddressSerializers(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status = 200)
+        return JsonResponse(serializer.errors, status = 400)
+
