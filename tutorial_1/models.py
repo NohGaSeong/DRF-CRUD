dc8b99f7993e8fd7bd931051c50eae0f62db8e95
yohan <noyohanx@naver.com>
2022-10-13 14:38:05 +0900

    update phoneNumber -> phone_number

--- tutorial_1/models.py
+++ tutorial_1/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Address(models.Model):
     name = models.CharField(max_length=10)
-    phoneNumber = models.CharField(max_length=15)
+    phone_number = models.CharField(max_length=15)
     address = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
