f1dba1e9bb2259435480748552289e9302ed0628
yohan <noyohanx@naver.com>
2022-10-14 10:02:30 +0900

    create different new Snippetserializer

--- tutorial_serializer/serializer.py
+++ tutorial_serializer/serializer.py
@@ -1,22 +1,29 @@
-from rest_framework import serializers
-from tutorial_serializer.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+ from rest_framework import serializers
+ from tutorial_serializer.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+#
+# 아무것도 안쓴. 순수 Serializer 을 구현하면 아래와 같은 코드가 나온다.
+# class SnippetSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+#     code = serializers.CharField(style={'base_template':'textarea.html'})
+#     linenos = serializers.BooleanField(required=False)
+#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+#
+#     def create(self, validated_data):
+#         return Snippet.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.code = validated_data.get('code', instance.code)
+#         instance.linenos = validated_data.get('linenos', instance.linenos)
+#         instance.language = validated_data.get('language', instance.language)
+#         instance.style = validated_data.get('style', instance.style)
+#         instance.save()
+#         return instance
 
-class SnippetSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    code = serializers.CharField(style={'base_template':'textarea.html'})
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-
-    def create(self, validated_data):
-        return Snippet.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
+# Model Serializer = 특별한 작업을 수행하지 않아도 단순히 serializer 클래스를 만드는 지름길
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id','title','code','linenos','language','style']
