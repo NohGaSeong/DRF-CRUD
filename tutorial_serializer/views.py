b7281eca615f59db4997c0cb6e9b64fe8e0959e9
yohan <noyohanx@naver.com>
2022-10-16 23:09:05 +0900

    update snippetDetail

--- tutorial_serializer/views.py
+++ tutorial_serializer/views.py
@@ -104,13 +104,16 @@ class SnippetDetail(mixins.RetrieveModelMixin,
                     mixins.UpdateModelMixin,
                     mixins.DestroyModelMixin,
                     generics.GenericAPIView):
+
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
     def get(self, request, *args, **kwargs):
-        return self.get(request, *args, **kwargs)
+        return self.retrieve(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
-        return self.put(request, *args, **kwargs )
+        return self.update(request, *args, **kwargs )
 
-    def destroy(self, request, *args, **kwargs):
+    def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
 
 
