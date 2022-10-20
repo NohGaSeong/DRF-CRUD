99711ad7690f493856a0e1f9e6d61c4bd39434c9
yohan <noyohanx@naver.com>
2022-10-20 20:24:18 +0900

    finish drf tutorial

--- tutorial_serializer/urls.py
+++ tutorial_serializer/urls.py
@@ -1,17 +1,14 @@
 from django.urls import path, include
-from tutorial_serializer import views
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+from snippets import views
 
+# router를 생성하고 viewset을 등록한다
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# API URL은 router에 의해 자동적으로 결정되어진다
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>', views.SnippetDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
-
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
-
-urlpatterns += [
-    path('api-auth/',include('rest_framework.urls')),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
 ]
--- tutorial_serializer/views.py
+++ tutorial_serializer/views.py
@@ -16,6 +16,7 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import permissions
+from rest_framework import renderers
 
 # Tutorial_1
 # @csrf_exempt
