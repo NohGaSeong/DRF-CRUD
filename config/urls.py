36eba2273492e6427c52d6e9d33c2cb938725aff
yohan <noyohanx@naver.com>
2022-10-27 11:14:19 +0900

    finish inflearn_part1

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,26 @@
+# from django.urls import path, include
+# from tutorial_1 import views
+#
+#
+# urlpatterns = [
+#     path('addresses/', views.address_list),
+#     path('addresses/<int:pk>/', views.address),
+#     path('login/',views.login),
+#     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+#     path('',include('tutorial_serializer.urls'))
+# ]
+
 from django.urls import path, include
-from tutorial_1 import views
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+from django.contrib import admin
 
 
 urlpatterns = [
-    path('addresses/', views.address_list),
-    path('addresses/<int:pk>/', views.address),
-    path('login/',views.login),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('',include('tutorial_serializer.urls'))
+    path('admin/', admin.site.urls),
+    path('api/',include('inflearnDjango.urls')),
+
+    # path('', include(router.urls)),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
